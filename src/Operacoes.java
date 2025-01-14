dd60b6948a08d4686544166f61bab6bcbc0bea32
lucas Frank <lucasfrank11@hotmail.com>
2025-01-14 15:11:35 -0300

    operacoes

---
+++ src/Operacoes.java
@@ -0,0 +1,21 @@
+/*
+Primeiro Comentario (ultimo tambem)
+ */
+
+
+public class Operacoes {
+
+    public static void main (String args[]) {
+        media();
+    }
+  public static void media(){
+      int numb1 = 1;
+      int numb2 =2;
+      int numb3 = 3;
+      int numb4 = 35;
+
+      int calculaMedia = numb1 + numb2 + numb3 + numb4 / 4;
+      System.out.println(calculaMedia);
+  }
+
+}
